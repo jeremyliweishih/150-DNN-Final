e05c6c743f052b99ee9d7bb09b76a7cb685c3a4c
Jeremy Shih <jeremyliweishih@gmail.com>
2019-04-23 15:16:06 -0400

    Fixed logpdf of normal

--- BBB_old/utils/BBBdistributions.py
+++ BBB_old/utils/BBBdistributions.py
@@ -38,7 +38,7 @@ class Normal(Distribution):
 
     def logpdf(self, x):
         c = - float(0.5 * math.log(2 * math.pi))
-        return return c - 0.5 * math.log(self.std) - (x - self.mu).pow(2) / (2 * self.std)
+        return c - 0.5 * torch.log(self.std) - (x - self.mu).pow(2) / (2 * self.std)
 
     def pdf(self, x):
         return torch.exp(self.logpdf(x))
@@ -64,7 +64,7 @@ class FixedNormal(Distribution):
 
     def logpdf(self, x):
         c = - float(0.5 * math.log(2 * math.pi))
-        return c - 0.5 * self.logvar - (x - self.mu).pow(2) / (2 * math.exp(self.logvar))
+        return c - 0.5 * torch.log(self.std) - (x - self.mu).pow(2) / (2 * self.std)
 
 
 class Normalout(Distribution):
