e01816628d8b07c86df2aa8f1684ce37c4237a59
Jeremy Shih <jeremyliweishih@gmail.com>
2019-04-23 15:07:47 -0400

    Debug KL

--- BBB_old/utils/BBBlayers.py
+++ BBB_old/utils/BBBlayers.py
@@ -152,9 +152,11 @@ class BBBConv2d(_ConvNd):
 
         # KL divergence
         qw_logpdf = self.conv_qw.logpdf(w_sample)
+        print("qw_logpdf:", qw_logpdf)
+        print("sample: ", self.pw.logpdf(w_sample))
 
         kl = torch.sum(qw_logpdf - self.pw.logpdf(w_sample))
-
+        print("kl :", kl)
         return output, kl
 
 
