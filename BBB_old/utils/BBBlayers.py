2ee5721e37c3138325006455e6b314f11a569b51
Jeremy Shih <jeremyliweishih@gmail.com>
2019-04-23 17:48:02 -0400

    get rid of prints

--- BBB_old/utils/BBBlayers.py
+++ BBB_old/utils/BBBlayers.py
@@ -151,8 +151,8 @@ class BBBConv2d(_ConvNd):
         # print('w_sample: ', w_sample)
         # KL divergence
         qw_logpdf = self.conv_qw.logpdf(w_sample)
-        print('qw_logpdf:', qw_logpdf)
-        print('pw_logpdf:', self.pw.logpdf(w_sample))
+        # print('qw_logpdf:', qw_logpdf)
+        # print('pw_logpdf:', self.pw.logpdf(w_sample))
 
         kl = torch.sum(qw_logpdf - self.pw.logpdf(w_sample))
         return output, kl
